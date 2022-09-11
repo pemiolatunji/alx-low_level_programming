167fbc4950e4abe8358febd564ccc280b682a3d5
pemiolatunji <pemiolatunji@ymail.com>
2022-09-11 10:30:59 -0700

    Betty edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+ * main - Entry point
+ * Return: always 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return(0);
+			return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+ * * main - Entry point
+ * Return: always 0
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
