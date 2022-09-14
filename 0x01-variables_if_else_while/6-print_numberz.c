01b62fd65517a2b6828a2b140433e5fe28024a1e
pemiolatunji <pemiolatunji@ymail.com>
2022-09-14 15:32:07 -0700

    Print numbrez

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * *main- Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	putchar(\n);
+	return (0);
+}
