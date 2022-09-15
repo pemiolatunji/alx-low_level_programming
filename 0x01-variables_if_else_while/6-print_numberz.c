5a953079de71440e0f9d72459ae8a44c1cfb4f6f
pemiolatunji <pemiolatunji@ymail.com>
2022-09-15 05:58:41 -0700

    Commit to print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * *main- Entry point
+ * *main - Write a program that prints all single digit numbers of base 10
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
